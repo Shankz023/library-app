d38615ba336eb6c82330fe784db905c27c64fb7b
shankz <official.shankarshaw@gmail.com>
2023-08-20 22:53:16 +0530

    Added vite run dev cmd, History Page for the books Loaned.

---
+++ src/models/HistoryModal.ts
@@ -0,0 +1,32 @@
+class HistoryModal {
+  id: number;
+  userEmail: string;
+  checkOutDate: string;
+  returnDate: string;
+  title: string;
+  author: string;
+  description: string;
+  img: string;
+
+  constructor(
+    id: number,
+    userEmail: string,
+    checkOutDate: string,
+    returnDate: string,
+    title: string,
+    author: string,
+    description: string,
+    img: string
+  ) {
+    this.id = id;
+    this.userEmail = userEmail;
+    this.checkOutDate = checkOutDate;
+    this.returnDate = returnDate;
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.img = img;
+  }
+}
+
+export default HistoryModal;
